eaef8048f9e8d8af09196fe976de6705a7386f9f
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-08-28 16:28:01 +0300

    feat: Post and get

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', name='World')
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello, {name}!'
+
+
+@app.route('/send_spare_part', methods=['POST'])
+def submit():
+    spare_part_name = request.form['spare_part_name']
+    spare_part_model = request.form['spare_part_model']
+    spare_part_price = request.form['spare_part_price']
+    seller_name = request.form['seller_name']
+    return {spare_part_name}
+
+@app.route('/get_spare_part', methods=['GET'])
+def submit():
+    spare_part_name = request.form['spare_part_name']
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
