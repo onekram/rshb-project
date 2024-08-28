289f3348b15bad384bd63ee366c9343a68705896
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-08-28 17:30:19 +0300

    fix: Finding by model

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import sqlite3
 app = Flask(__name__)
 
@@ -59,11 +59,30 @@ def success():
 
 @app.route('/get_spare_part', methods=['GET'])
 def get():
-    spare_part_model = request.args.get('spare_part_model', '')
-    conn = get_db_connection()
-    parts = conn.execute('SELECT * FROM parts WHERE spare_part_name LIKE ?', ('%' + spare_part_model + '%',)).fetchall()
-    conn.close()
-    return render_template('search_results.html', users=parts)
+
+    model = request.args.get('spare_part_model')  # Получаем модель из параметров запроса
+
+    if not model:
+        return jsonify({'error': 'Не указана модель'}), 400
+
+    # Выполняем поиск в базе данных
+    with get_db_connection() as conn:
+        parts = conn.execute('''
+            SELECT * FROM parts WHERE spare_part_model = ? ORDER BY spare_part_price
+        ''', (model,)).fetchall()
+
+    # Формируем ответ
+    results = []
+    for part in parts:
+        results.append({
+            'id': part['id'],
+            'spare_part_name': part['spare_part_name'],
+            'spare_part_model': part['spare_part_model'],
+            'spare_part_price': part['spare_part_price'],
+            'seller_name': part['seller_name']
+        })
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     init_db()
