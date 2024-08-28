7c608ded9a74ac4b011e43d0a9f0ad540da03915
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-08-28 17:04:33 +0300

    feat: Add part to db on post

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 app = Flask(__name__)
 
+# Функция для подключения к базе данных
 def get_db_connection():
-    conn = sqlite3.connect('users.db')
-    conn.row_factory = sqlite3.Row
+    conn = sqlite3.connect('parts.db')
+    conn.row_factory = sqlite3.Row  # Позволяет обращаться к столбцам по имени
     return conn
 
+# Создаем таблицу, если она не существует
 def init_db():
-    conn = get_db_connection()
-    conn.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
-    conn.commit()
-    conn.close()
+    with get_db_connection() as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS parts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                spare_part_name TEXT NOT NULL,
+                spare_part_model TEXT NOT NULL,
+                spare_part_price REAL NOT NULL,
+                seller_name TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
 
 @app.route('/seller')
 def seller():
@@ -33,7 +42,16 @@ def send():
     spare_part_model = request.form['spare_part_model']
     spare_part_price = request.form['spare_part_price']
     seller_name = request.form['seller_name']
-    return f'{spare_part_name}'
+
+    # Вставляем данные в таблицу
+    with get_db_connection() as conn:
+        conn.execute('''
+            INSERT INTO parts (spare_part_name, spare_part_model, spare_part_price, seller_name)
+            VALUES (?, ?, ?, ?)
+        ''', (spare_part_name, spare_part_model, spare_part_price, seller_name))
+        conn.commit()
+
+    return 'message: Запчасть добавлена успешно!'
 
 @app.route('/get_spare_part', methods=['GET'])
 def get():
@@ -44,4 +62,5 @@ def get():
     return render_template('search_results.html', users=users)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
